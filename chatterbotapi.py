88c785a25c0d0f6a8075cb0340ade924fe3cb43d
Adrinus <mckibbon@gmail.com>
2015-07-31 21:44:05 -0400

    Update chatterbotapi.py

--- chatterbotapi.py
+++ chatterbotapi.py
@@ -37,9 +37,9 @@ class ChatterBotFactory:
 
     def create(self, type, arg = None):
         if type == ChatterBotType.CLEVERBOT:
-            return _Cleverbot('http://www.cleverbot.com', 'http://www.cleverbot$
+             return _Cleverbot('http://www.cleverbot.com', 'http://www.cleverbot.com/webservicemin', 35)
         elif type == ChatterBotType.JABBERWACKY:
-            return _Cleverbot('http://jabberwacky.com', 'http://jabberwacky.com$
+            return _Cleverbot('http://jabberwacky.com', 'http://jabberwacky.com/webservicemin', 29)
         elif type == ChatterBotType.PANDORABOTS:
             if arg == None:
                 raise Exception('PANDORABOTS needs a botid arg')
@@ -91,7 +91,7 @@ class _CleverbotSession(ChatterBotSession):
         self.vars['islearning'] = '1'
         self.vars['cleanslate'] = 'false'
         self.cookieJar = cookielib.CookieJar()
-        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.coo$
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookieJar))
         self.opener.open(self.bot.baseUrl)
 
     def think_thought(self, thought):
@@ -115,7 +115,7 @@ class _CleverbotSession(ChatterBotSession):
         self.vars['vText2'] = _utils_string_at_index(response_values, 9)
         self.vars['prevref'] = _utils_string_at_index(response_values, 10)
         #self.vars['??'] = _utils_string_at_index(response_values, 11)
-        self.vars['emotionalhistory'] = _utils_string_at_index(response_values,$
+        self.vars['emotionalhistory'] = _utils_string_at_index(response_values, 12)
         self.vars['ttsLocMP3'] = _utils_string_at_index(response_values, 13)
         self.vars['ttsLocTXT'] = _utils_string_at_index(response_values, 14)
         self.vars['ttsLocTXT3'] = _utils_string_at_index(response_values, 15)
@@ -124,7 +124,7 @@ class _CleverbotSession(ChatterBotSession):
         self.vars['lineURL'] = _utils_string_at_index(response_values, 18)
         self.vars['linePOST'] = _utils_string_at_index(response_values, 19)
         self.vars['lineChoices'] = _utils_string_at_index(response_values, 20)
-        self.vars['lineChoicesAbbrev'] = _utils_string_at_index(response_values$
+        self.vars['lineChoicesAbbrev'] = _utils_string_at_index(response_values, 21)
         self.vars['typingData'] = _utils_string_at_index(response_values, 22)
         self.vars['divert'] = _utils_string_at_index(response_values, 23)
         response_thought = ChatterBotThought()
@@ -153,21 +153,22 @@ class _PandorabotsSession(ChatterBotSession):
     def think_thought(self, thought):
         self.vars['input'] = thought.text
         data = urllib.urlencode(self.vars)
-        url_response = urllib2.urlopen('http://www.pandorabots.com/pandora/talk$
+        url_response = urllib2.urlopen('http://www.pandorabots.com/pandora/talk-xml', data)
         response = url_response.read()
         response_dom = xml.dom.minidom.parseString(response)
         response_thought = ChatterBotThought()
         that_elements = response_dom.getElementsByTagName('that')
-        if that_elements is None or len(that_elements) == 0 or that_elements[0]$
+        if that_elements is None or len(that_elements) == 0 or that_elements[0] is None:
             return ''
         that_elements_child_nodes = that_elements[0].childNodes
-        if that_elements_child_nodes is None or len(that_elements_child_nodes) $
+        if that_elements_child_nodes is None or len(that_elements_child_nodes) == 0 or that_elements_child_nodes[0] is None:
             return ''
         that_elements_child_nodes_data = that_elements_child_nodes[0].data
         if that_elements_child_nodes_data is None:
             return ''
         response_thought.text = that_elements_child_nodes_data.strip()
         return response_thought
+
 
 #################################################
 # Utils
